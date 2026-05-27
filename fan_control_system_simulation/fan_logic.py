4681c2a36d00efdc5095f1581c4a894616ce6f43
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-27 23:46:00 +0800

    feat: initialize fan calss with encapsulated attributes
    - add fan class with class variable (SLOW, MEDIUM, FAST)
    - __init__ constructor with parameters (speed, radius, color, and power)
    - Access modifiers used for attribute is private

--- fan_control_system_simulation/fan_logic.py
+++ fan_control_system_simulation/fan_logic.py
@@ -1,2 +1,12 @@
 # Fan Logic
 
+class Fan: 
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+    
+    def __init__(self, speed=None, radius = 10.0, color="white", on=False):
+        self.__speed = speed
+        self.__radius = float(radius)
+        self.__color = color
+        self.__on = on
