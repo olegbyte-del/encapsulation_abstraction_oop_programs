99af39301559f0de0fb80da4ef264126a9b76528
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 13:26:46 +0800

    Download gif files for video playback and create GIFPlayer class - intilize label for displaying gif, clock_timer for delay and frames for storing gif - imported gif files

--- fan_control_system_simulation/video_engine.py
+++ fan_control_system_simulation/video_engine.py
@@ -1,1 +1,11 @@
 # video_engine
+
+import tkinter as tk
+
+class GIFPlayer:
+    def __init__(self, display_screen, clock_timer):
+        self.label = display_screen
+        self.root = clock_timer
+        self.frames = []
+        self.idx = 0
+        self.job = None
