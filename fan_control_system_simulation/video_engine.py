298391172a819d2b5edcb0ca2944f5965a195153
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 13:46:16 +0800

    fix: add docstring and correct syntx and spelling errors
    - add docstring for the video_engine describing the class and method
    - fix incorrect syntax and GIF loading

--- fan_control_system_simulation/video_engine.py
+++ fan_control_system_simulation/video_engine.py
@@ -3,8 +3,11 @@
 import tkinter as tk
 
 class GIFPlayer:
+    """Initialize animated GIF playback"""
+    
     #initializing class for video playback
     def __init__(self, display_screen, clock_timer):
+        """Initialize constructor as well as parameters and specific directory of files"""
         self.label = display_screen
         self.root = clock_timer
         self.frames = []
@@ -21,18 +24,21 @@ class GIFPlayer:
         }
     
     def load_gif(self, path): 
+        """ Extract all individual from the specified GIF file path"""
+        
         self.frames = []
         file_index = 0
         
         while True:
-            Try:
-                frame = tk.PhotoImage(file=self.path, format = f"gif -index {file_index}")
-                self.frames.ppend(frame)
+            try:
+                frame = tk.PhotoImage(file=path, format = f"gif -index {file_index}")
+                self.frames.append(frame)
                 file_index += 1
             except:
                 break
             
     def loop(self):
+        """Cycle through the GIF continously"""
         # loops the current gif that were selected
         self.label.config(image=self.frames[self.idx])
         self.idx = (self.idx + 1) % len(self.frames)
