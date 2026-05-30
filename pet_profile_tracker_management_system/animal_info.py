8f247f5c951043fc4624dce79b8c5474609bc955
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 22:37:15 +0800

    feat: add getter methods for Pet class attributes and add id number attribute
    - add getter methods for name, animal type, and age
    - add id_number generator attribute for ID creation feature

--- pet_profile_tracker_management_system/animal_info.py
+++ pet_profile_tracker_management_system/animal_info.py
@@ -1,11 +1,12 @@
 # Animal Information
-
+import random
 class Pet:
 
     def __init__(self, name = "", animal_type= "", age=0):
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+        self.__id_number = f"PET-{random.randint(1000, 9999)}"
     
     # Setter
     def set_age(self, age):
@@ -21,6 +22,16 @@ class Pet:
     
     def set_name(self, name):
         self.__name = name.title()
-                
-            
     
+    # Getter
+    def get_name(self):
+        return self.__name 
+    
+    def get_animal_type(self):
+        return self.__animal_type
+    
+    def get_age(self):
+        return self.__age
+    
+    def get_id_number(self): 
+        return self.__id_number
