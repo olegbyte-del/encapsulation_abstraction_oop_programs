75fb26a75c6dfb8cdceef95b09bc1c0a1d6598ba
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 22:57:21 +0800

    feat: add input frame layout for Pet ID registration form
    - add input frame method for GUI layout
    - creat labelframe for pet registration

--- pet_profile_tracker_management_system/pet_gen_system.py
+++ pet_profile_tracker_management_system/pet_gen_system.py
@@ -11,4 +11,12 @@ class PetAppGui:
         self.root.geometry("500x500")
         self.root.configure(bg="#DBA674")
         
-        
+    def input_frame(self):
+        input_frame = tk.LabelFrame(self.root,
+            text = "Register Your pet",
+            font = ("Cosmic Sans MS", 12, "bold"),
+            padx=15,
+            pady= 15,
+            bg= "#000000"
+        )
+        input_frame.place(x=20, y=20, width=320, height=400)
