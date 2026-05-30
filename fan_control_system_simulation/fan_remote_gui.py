2c565a7265689878504e8328da5ae352ca9cd934
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 11:58:38 +0800

    feat: add status display screen for fan remote
    - added tkinter string var deafult to display the speed of the fan
    - screen label for dynamic status update

--- fan_control_system_simulation/fan_remote_gui.py
+++ fan_control_system_simulation/fan_remote_gui.py
@@ -12,7 +12,21 @@ class FanRemoteGUI:
         
         self.fan = Fan()
         
-        self.screen_var = tk.StringVar()
+        self.screen_var = tk.StringVar(value="Fan off")
+        self.screen = tk.Label(
+            self.root, 
+            textvariable=self.screen_var, 
+            font=("Arial", 16, "bold"), 
+            bg="#A0C4DF", 
+            fg="#1A365D", 
+            height=3, 
+            width=20, 
+            relief="sunken", 
+            bd=4
+        ) 
+        self.screen.pack(pady=25)
         
         self.logo_img = tk.PhotoImage(file=r"D:\PUP\First year - Second Semester\Object Oriented Programming\encapsulation_abstraction_oop_programs\fan_control_system_simulation\remote.png")
         self.root.iconphoto(False, self.logo_img)
+        
+        
