eda1483c6cdf92af42e97b9e3bfba57e25089c7a
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 22:52:09 +0800

    feat: Imported necessary files and add attribute in the constructor
    - Imported tkinter, filedialog, messagebox, and pillow
    - add attribute to the constructor

--- pet_profile_tracker_management_system/pet_gen_system.py
+++ pet_profile_tracker_management_system/pet_gen_system.py
@@ -1,6 +1,14 @@
 # Gui Application for pet ID generator
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
 
 class PetAppGui:
     
-    def __init__(self):
-        pass
+    def __init__(self, root):
+        self.root = root 
+        self.root.title("Pet ID Profile Generator")
+        self.root.geometry("500x500")
+        self.root.configure(bg="#DBA674")
+        
+        
