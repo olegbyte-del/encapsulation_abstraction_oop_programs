a7d2ca4a58128f37f5ce8db7038f5dc279d99cd1
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 12:08:20 +0800

    feat: redesign Fan Remote GUI layout and added speed control buttons
    - add documentation comments for each code #
    - Updated GUI window size and color theme
    - added interactive buttons for low, meidum and high fan speeds and changes in the screen display.

--- fan_control_system_simulation/fan_remote_gui.py
+++ fan_control_system_simulation/fan_remote_gui.py
@@ -5,28 +5,44 @@ from fan_logic import Fan
 
 class FanRemoteGUI:
     def __init__(self, root):
+        # Structure of the GUI
         self.root = root
         self.root.title("Fan Remote")
-        self.root.geometry("500x500")
-        self.root.configure(bg="#2C5E90")
+        self.root.geometry("300x500")
+        self.root.configure(bg="#2A4D6F")
         
+        # Icon logo for the remote
+        self.logo_img = tk.PhotoImage(file=r"D:\PUP\First year - Second Semester\Object Oriented Programming\encapsulation_abstraction_oop_programs\fan_control_system_simulation\remote.png")
+        self.root.iconphoto(False, self.logo_img)
+        
+        # Initialize object fan from fan_logic
         self.fan = Fan()
         
+        # Screen Display
         self.screen_var = tk.StringVar(value="Fan off")
         self.screen = tk.Label(
             self.root, 
             textvariable=self.screen_var, 
             font=("Arial", 16, "bold"), 
-            bg="#A0C4DF", 
-            fg="#1A365D", 
-            height=3, 
+            bg="#FDFDFD", 
+            fg="#414141", 
+            height=2, 
             width=20, 
             relief="sunken", 
             bd=4
         ) 
         self.screen.pack(pady=25)
         
-        self.logo_img = tk.PhotoImage(file=r"D:\PUP\First year - Second Semester\Object Oriented Programming\encapsulation_abstraction_oop_programs\fan_control_system_simulation\remote.png")
-        self.root.iconphoto(False, self.logo_img)
+        # Buttons for the speeds
+        mode = {1: "1 Low", 2: "2 Medium", 3: "3 High"}
+        
+        for speed, speed_status in mode.items():
+            button = tk.Button(self.root
+                text = speed_status
+                font=("Arial", 12, "Bold")
+                width=20,
+                height=3,
+                action = lambda s=speed: self.set_fan_speed(s)
+            ) btn.pack(pady=6)
         
         
