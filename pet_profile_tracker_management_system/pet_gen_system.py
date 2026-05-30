876194d62a515fe04230280685186509f7df3bfb
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 23:07:50 +0800

    feat: improve input frame and add pet name entry field
    - update window geometry
    - displayed input_frame using self.input_frame()
    - Add Pet name label and entry field for user input

--- pet_profile_tracker_management_system/pet_gen_system.py
+++ pet_profile_tracker_management_system/pet_gen_system.py
@@ -8,15 +8,21 @@ class PetAppGui:
     def __init__(self, root):
         self.root = root 
         self.root.title("Pet ID Profile Generator")
-        self.root.geometry("500x500")
+        self.root.geometry("365x425")
         self.root.configure(bg="#DBA674")
+        
+        self.input_frame()
         
     def input_frame(self):
         input_frame = tk.LabelFrame(self.root,
-            text = "Register Your pet",
+            text = "Register Your pet".center(60),
             font = ("Cosmic Sans MS", 12, "bold"),
             padx=15,
             pady= 15,
-            bg= "#000000"
+            bg= "#D8C8C8"
         )
-        input_frame.place(x=20, y=20, width=320, height=400)
+        input_frame.place(x=20, y=20, width=320, height=400)\
+        
+        tk.Label(input_frame, text = "Pet Name:", bg = "#FFFFFF").pack(anchor="w")
+        self.pet_name = tk.Entry(input_frame, font=("Cosmic Sans MS", 11))
+        self.pet_name.pack(fill="x", pady=(0,10))
