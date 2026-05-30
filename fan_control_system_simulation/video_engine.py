ab610c8b463b8b8d9872f8f1a40518ce3f81c62e
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 13:35:37 +0800

    feat: ad GIF path mapping and loop method
    - add specific file directory for gif file for fan levels
    - add loop method where it loops the current gif selected

--- fan_control_system_simulation/video_engine.py
+++ fan_control_system_simulation/video_engine.py
@@ -3,9 +3,25 @@
 import tkinter as tk
 
 class GIFPlayer:
+    #initializing class for video playback
     def __init__(self, display_screen, clock_timer):
         self.label = display_screen
         self.root = clock_timer
         self.frames = []
         self.idx = 0
         self.job = None
+        
+        # setting exact paths for gif files
+        base = r"D:\PUP\First year - Second Semester\Object Oriented Programming\encapsulation_abstraction_oop_programs\fan_control_system_simulation\\"
+        self.paths = {
+            1: base + "low.gif",
+            2: base + "medium.gif",
+            3: base + "fast.gif",
+            "off": base + "power off.gif"
+        }
+    
+    def loop(self):
+        # loops the current gif that were selected
+        self.label.config(image=self.frames[self.idx])
+        self.idx = (self.idx + 1) % len(self.frames)
+        self.job = self.root.after(50, self. loop)
