9c2a1d3d4b998db695490d7093deaf7ee560465f
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 22:21:45 +0800

    feat: create animal_info file and initialize attributes
    - created pet class to store name, animal type, and age
    - implement private attributes using encapsulation

---
+++ pet_profile_tracker_management_system/animal_info.py
@@ -0,0 +1,9 @@
+# Animal Information
+
+class Pet:
+
+    def __init__(self, name = "", animal_type= "", age=0):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+    
