4f44508539c385d11db5b9f5fc1bd49c6c76ee4c
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-30 22:30:16 +0800

    feat: add setters with validation and format for pet class
    - added set_age, set_name, and set_animal_type setters
    - add condition in set_agge and format name using title()

--- pet_profile_tracker_management_system/animal_info.py
+++ pet_profile_tracker_management_system/animal_info.py
@@ -7,3 +7,20 @@ class Pet:
         self.__animal_type = animal_type
         self.__age = age
     
+    # Setter
+    def set_age(self, age):
+        try:
+            if age < 0:
+                print("Age cannot be less than zero!",
+                    "Setting age to N/A", sep = "\n")
+        except Exception as e:
+            print('Error Occured')
+    
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+    
+    def set_name(self, name):
+        self.__name = name.title()
+                
+            
+    
